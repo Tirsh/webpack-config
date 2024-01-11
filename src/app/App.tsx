ca1ea9d8b081dd5cc2736761a3e9d7ad1ba0a84f
Tirsh <valeriy.vasipov@gmail.com>
2024-01-11 17:18:53 +0300

    Sidebar. Layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,18 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
     const {theme} = useTheme();
     return (
         <div className={classNames('app', {hovered: true, selected: false}, [theme])}>
             <Navbar/>
-            <AppRouter/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
+
         </div>
     );
 };
